ede0256719f86738852a85e660070d81e03b505f
kixyu9527 <kixyu9527@gmail.com>
2026-08-01 14:07:06 +0800

    chore(release): 将版本提升至 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.kixyu9527.kixyubook"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1088
-        versionName = "2.0.0"
+        versionCode = 1089
+        versionName = "2.1.0"
     }
 
     val externalSigningFile = file(
